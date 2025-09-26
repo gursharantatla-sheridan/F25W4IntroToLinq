73a552d6de2bd9f5dd6c7ae2f4672614e00654ab
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-26 14:45:19 -0400

    deferred exec

--- F25W4IntroToLinq/Program.cs
+++ F25W4IntroToLinq/Program.cs
@@ -6,15 +6,46 @@
         {
             int[] array = { 4, 3, 5, 6, 2, 1, 7, 8, 9, 5, 6, 7, 4 };
 
+            // query syntax
             var greaterThan4 = from n in array
                                where n > 4
+                               orderby n
                                select n;
+
+            // method syntax
+            greaterThan4 = array.Where(n => n > 4).OrderBy(n => n);
 
             foreach (var i in greaterThan4)
                 Console.Write(i + " ");
+            Console.WriteLine("\n\n\n\n");
+
+
+
+
+            List<string> colors = new List<string>();
+            colors.Add("bLuE");
+            colors.Add("ruSt");
+            colors.Add("GreeN");
+            colors.Add("ReD");
+
+            var startsWithR = from c in colors
+                              let uppercaseColors = c.ToUpper()
+                              where uppercaseColors.StartsWith("R")
+                              orderby uppercaseColors
+                              select uppercaseColors;
+
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
             Console.WriteLine("\n\n");
 
 
+            colors.Add("rUbY");
+            colors.Add("PinK");
+
+            // deferred execution
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
         }
     }
 }
